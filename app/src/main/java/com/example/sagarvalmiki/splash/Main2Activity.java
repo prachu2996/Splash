7dbf1e96ece080ce3f073a96bd888664cadf4f95
prachu2996 <prachi.walmiki2996@gmail.com>
2018-02-01 03:09:09 +0530

    Initial commit

---
+++ app/src/main/java/com/example/sagarvalmiki/splash/Main2Activity.java
@@ -0,0 +1,30 @@
+package com.example.sagarvalmiki.splash;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        Thread myThread = new Thread(){
+            @Override
+            public void run(){
+                try{
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch(InterruptedException e){
+                    e.printStackTrace();
+                }
+
+            }
+
+        };
+        myThread.start();
+    }
+}
